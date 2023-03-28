6420445b47a67307fe942dce4bbb9f81fcc6b5c3
Radul19 <radulito19@gmail.com>
2023-03-28 03:17:30 -0400

    more clean

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,7 @@ import {
 
 import { interpolate, splitPathString, interpolateAll } from "flubber";
 // const paths = [arrGamepad, arrBox, arrHome, arrUser];
-// const paths = [gamepad, box, home, user];
-// const wtf = [0,1,2,3,4,5]
+const paths = [gamepad, box, home, user];
 
 const rand = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
@@ -44,8 +43,7 @@ const randomLine = () => {
 
 function App() {
   const [paths, setPaths] = useState([randomLine(), randomLine()]);
-  const [pathIndex, setPathIndex] = useState(1);
-  const progress = useMotionValue(pathIndex-1);
+  const progress = useMotionValue(0);
   const path = useTransform(progress, [0, 1], paths, {
     mixer: (a, b) => {
       return interpolate(a, b, { maxSegmentLength: 10 });
@@ -57,44 +55,50 @@ function App() {
   }, [paths]);
 
   const advance = () => {
-    animate(progress, pathIndex, {
+    animate(progress, 1, {
       duration: 1.5,
       onComplete: () => {
         setPaths((prev) => [prev[1], randomLine()]);
-
-        setPathIndex(1);
       },
     });
   };
 
   return (
     <div className="root">
-      <div className="box">
-        <svg width="300" height="300" onClick={advance}>
-          <motion.path d={path} stroke="#fff" fill="none" strokeLinejoin={"round"} />
+      <motion.div className="box" onClick={advance}>
+        <svg width="300" height="300" fill="white">
+          <motion.path
+            d={path}
+            stroke="#fff"
+            fill="none"
+            strokeLinejoin={"round"}
+          />
         </svg>
-      </div>
-      {/* <motion.div
-        // drag="x"
-        style={{ x: spring }}
-        className="boxAbs"
-        onClick={changev}
-      ></motion.div> */}
+      </motion.div>
     </div>
   );
 }
 
 export default App;
 /**
- * {interpolators.path.map((item, index) => (
-            <animated.path
-              key={index}
-              d={animationProps.x.to(item)}
-              stroke="#fff"
-              fill="none"
-              strokeWidth={4}
-            ></animated.path>
-          ))}
+  // const [paths, setPaths] = useState([randomLine(), randomLine()]);
+  // const [pathIndex, setPathIndex] = useState(1);
+  // const progress = useMotionValue(pathIndex-1);
+  // const path = useTransform(progress, [0, 1], paths);
+
+  // useEffect(() => {
+  //   progress.set(0);
+  //   advance()
+  // }, [paths]);
+
+  // const advance = () => {
+  //   animate(progress, pathIndex, {
+  //     duration: 0.8,
+  //     onComplete: () => {
+  //       setPaths((prev) => [prev[1], randomLine()]);
+  //       setPathIndex(1);
+  //       // advance()
+  //     },
+  //   });
+  // };
  */
-
-
