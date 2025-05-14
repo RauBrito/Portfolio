2b234180d42c47eec01cb8c2f19a426b9285f885
Radul19 <radulito19@gmail.com>
2025-05-14 09:48:41 -0400

    paddings to home and lines

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,13 @@ import pfp from "/pfptemp.png";
 const Home = () => {
   return (
     <div className="w-screen bg-neutral-900 flex">
-      <div className="flex-1 flex flex-col gap-6 p-6 items-center md:min-h-[90vh] md:flex-row md:justify-evenly">
+      <div className="relative flex-1 flex flex-col gap-6 p-6 pb-24 items-center md:min-h-[90vh] md:flex-row md:justify-evenly md:pb-6">
         <Left />
         <Center />
         <Right />
+        <div className="absolute bottom-6">
+          <Lines />
+        </div>
       </div>
     </div>
   );
@@ -17,7 +20,9 @@ const Left = () => {
   return (
     <div className="flex justify-center w-full md:w-1/4 md:flex-col md:justify-between md:min-h-[320px]">
       <div className="text-red-100 flex flex-col gap-4 ">
-        <p className="font-[MatrixMono] text-center text-2xl md:text-left">raul brito</p>
+        <p className="font-[MatrixMono] text-center text-2xl md:text-left">
+          raul brito
+        </p>
         <p className="font-[MatrixMono] text-center text-sm  md:text-left">
           full stack <br />
           web developer
@@ -56,56 +61,18 @@ const Right = () => {
   );
 };
 
-/**
- * 
-const Home = () => {
+const Lines = () => {
   return (
-    <div className="w-screen h-[93vh] bg-black flex flex-col">
-      <div className="flex-1 flex justify-between items-center px-48 bg-neutral-950">
-        <Left />
-        <Left />
-        <Left />
-      </div>
-    </div>
+    <svg
+      width="42"
+      height="14"
+      viewBox="0 0 42 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line y1="1" x2="42" y2="1" stroke="white" stroke-width="2" />
+      <line x1="9" y1="7" x2="33" y2="7" stroke="white" stroke-width="2" />
+      <line x1="17" y1="13" x2="25" y2="13" stroke="white" stroke-width="2" />
+    </svg>
   );
 };
-export default Home;
-
-const Left = () => {
-  return (
-    <div className="w-1/4 h-[350px] flex flex-col justify-between pt-20 ">
-      <div className="flex flex-col gap-2 text-red-100">
-        <p style={{ fontFamily: "MatrixMono" }}>raul brito</p>
-        <p style={{ fontFamily: "MatrixMono" }}>web developer</p>
-      </div>
-      <div className="">
-        <BtnV2 text="my cv" pcolor="red-100" scolor="neutral-950" />
-      </div>
-    </div>
-  );
-};
-
-const Center = () => {
-  return (
-    <div className="w-1/4 h-[500px] bg-neutral-800/20 ">
-      <p>a</p>
-    </div>
-  );
-};
-const Right = () => {
-  return (
-    <div className="w-1/4 h-[350px] flex flex-col justify-between pt-20 items-end ">
-      <p
-        style={{ fontFamily: "MatrixMono" }}
-        className="text-[10px]/5 linehe text-red-100 text-right"
-      >
-        Programador especializado en la web y aplicaciones mobiles.
-        Apasionado por resolver problemas y afrontar desafios, siempre buscando
-        la innovacion y eficacia.
-      </p>
-        <BtnV2 text="my cv" pcolor="red-100" scolor="neutral-950" />
-    </div>
-  );
-};
-
- */
